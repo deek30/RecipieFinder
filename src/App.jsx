42d59c4c80cdd724d4f18bf92f9a84ee660f46c7
deek30 <deek3003@gmail.com>
2025-04-18 00:30:05 +0530

    app.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App=()=>{
+  const [query, setQuery] = useState(' ');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(' ');
+  const [recipies, setRecipies] = useState([]);
+
+  const searchRecipies = async() => {
+    if(!query) return;
+    setLoading(true);
+    setError('');
+    try{
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+      const data = await res.json90;
+      if(data.meals){
+        setRecipies(data.meals);
+      }
+      else{
+        setRecipies([]);
+        setError('No meals found');
+      }
+    }
+    catch(err){
+      setLoading('Failed to fetch recipies');
+    }
+    finally{
+      setLoading(false);
+    }
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{padding:'2rem', fontFamily:'Arial'}}>
+      <h1> Recipieee Finder</h1>
+      <input
+        type = "text"
+        placeholder="Search for a meal..."
+        value={query}
+        onChange={(e)=>setQuery(e.target.value)}
+      />
+      <button onClick={searchRecipies}>Search</button>
 
-export default App
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      {/* <div style={{display:'flex', flexWrap:'wrap', gap:'1rem'}}>
+        {recipies.map((meal)=>(
+          <RecipieCard key = {meal.idMeal} meal={meal}/>
+        ))}
+
+      </div> */}
+    </div>
+  );
+};
+export default App;
